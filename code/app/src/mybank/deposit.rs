4a8fb2fbb5e668ed44f17e548294d11bfaf65786
Miro Svrtan <miro@mirosvrtan.me>
2019-03-30 08:15:32 +0100

    Add handler for depositing money

---
+++ code/app/src/mybank/deposit.rs
@@ -0,0 +1,28 @@
+use super::prelude::*;
+use std::sync::Arc;
+
+pub struct DepositHandler<T>
+where
+    T: BankAccountRepository,
+{
+    pub repository: Arc<T>,
+}
+
+impl<T: BankAccountRepository> DepositHandler<T> {
+    pub fn handle(&self, command: DepositPayload) -> Result<(), Error> {
+        let repo = Arc::clone(&self.repository);
+
+        let current_events = repo.get_events();
+        let initial_state = BankAccountAggregate::apply_events(current_events.unwrap());
+
+        let result: Result<Vec<BankAccountEvent>, Error> =
+            BankAccountAggregate::deposit(initial_state.unwrap(), command);
+
+        let events = result?;
+
+        match repo.save_events(events) {
+            Ok(()) => Ok(()),
+            _ => Err(Error::CantSaveEvent),
+        }
+    }
+}
