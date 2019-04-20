13b0e3fea302f621843cabb62a09e76100b1d170
Miro Svrtan <miro@mirosvrtan.me>
2019-04-20 20:48:36 +0200

    Cleaning up es lib

--- code/eventsourcing/src/lib.rs
+++ code/eventsourcing/src/lib.rs
@@ -23,37 +23,19 @@ pub trait Event {
 }
 
 pub trait AggregateEvent<A: Aggregate>: Event {
-    /// The error type.
     type Error: CqrsError;
     fn apply_to(self, aggregate: &mut A) -> Result<(), Self::Error>;
 }
 
-/// A command that can be executed against an aggregate.
 pub trait AggregateCommand<A: Aggregate> {
-    /// The type of event that is produced by this command.
     type Event: AggregateEvent<A>;
-
-    /// The type of the sequence of events generated when the command is executed successfully.
     type Events: Events<ProducedEvent<A, Self>>;
-
-    /// The error type.
     type Error: CqrsError;
-
-    /// Consumes a command, attempting to execute it against the aggregate. If the execution is successful, a sequence
-    /// of events is generated, which can be applied to the aggregate.
     fn execute_on(self, aggregate: &A) -> Result<Self::Events, Self::Error>;
 }
 
-/// The event type produced by this command.
 pub type ProducedEvent<A, C> = <C as AggregateCommand<A>>::Event;
 
-/// The event sequence produced by this command.
-pub type ProducedEvents<A, C> = <C as AggregateCommand<A>>::Events;
-
-/// The error produced when this command cannot be executed against an aggregate.
-pub type CommandError<A, C> = <C as AggregateCommand<A>>::Error;
-
-/// An iterable and sliceable list of events.
 pub trait Events<E>: IntoIterator<Item = E> + AsRef<[E]>
 where
     E: Event,
@@ -67,7 +49,6 @@ where
 {
 }
 
-/// Represents a common trait that all errors handled by CQRS should implement.
 pub trait CqrsError: fmt::Debug + fmt::Display + Send + Sync + 'static {}
 
 impl<T> CqrsError for T where T: fmt::Debug + fmt::Display + Send + Sync + 'static {}
